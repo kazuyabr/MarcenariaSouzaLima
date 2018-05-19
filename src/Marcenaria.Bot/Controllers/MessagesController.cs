9ebce8b8353819bdcbffc75c647a59a0872bd8cf
Jailton Júnior <jailton.junior94@outlook.com>
2018-05-19 12:45:37 -0300

    Add project files.

---
+++ src/Marcenaria.Bot/Controllers/MessagesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Net.Http;
+using System.Configuration;
+using Marcenaria.Bot.Dialogs;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace Marcenaria.Bot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            var Connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            var Attributes = new LuisModelAttribute(ConfigurationManager.AppSettings["LuisId"], ConfigurationManager.AppSettings["LuisSubscriptionKey"]);
+            var Service = new LuisService(Attributes);
+
+            switch (activity.Type)
+            {
+                case ActivityTypes.Message:
+                    await Conversation.SendAsync(activity, () => new RootDialog(Service));
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                    if (activity.MembersAdded.Any(o => o.Id == activity.Recipient.Id))
+                    {
+                        var reply = activity.CreateReply();
+                        reply.Text = Welcome();
+
+                        await Connector.Conversations.ReplyToActivityAsync(reply);
+                    }
+                    break;
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        private string Welcome()
+        {
+            var Now = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time")).TimeOfDay;
+            string Welcome;
+
+            if (Now < TimeSpan.FromHours(12))
+                Welcome = "Bom dia";
+            else if (Now < TimeSpan.FromHours(18))
+                Welcome = "Boa tarde";
+            else
+                Welcome = "Boa noite";
+
+            return $"{Welcome}! Eu sou o Botinho Souza Lima, Em que posso ajudar?";
+        }
+    }
+}
---
+++ src/Marcenaria.Bot/Dialogs/RootDialog.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Marcenaria.Bot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : LuisDialog<object>
+    {
+        public RootDialog(ILuisService service) : base(service) { }
+
+        [LuisIntent("None")]
+        public async Task NoneAsync(IDialogContext context, LuisResult luisResult)
+        {
+            await context.PostAsync("Desculpe, eu não entendi...\n");
+            context.Done<string>(null);
+        }
+
+        [LuisIntent("About")]
+        public async Task AboutAsync(IDialogContext context, LuisResult luisResult)
+        {
+            //await context.PostAsync(AboutConstant.About);
+            context.Done<string>(null);
+        }
+
+        [LuisIntent("Projects")]
+        public async Task ProjectsAsync(IDialogContext context, LuisResult luisResult)
+        {
+            //var Carousel = new CreateCardService().CreateCarrouselImages();
+            //var Message = context.MakeMessage();
+
+            //Message.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            //Message.Attachments.Add(Carousel);
+
+            await context.PostAsync("");
+            context.Done<string>(null);
+        }
+
+        [LuisIntent("ProjectDetail")]
+        public async Task ProjectDetailAsync(IDialogContext context, LuisResult luisResult)
+        {
+            var Projects = luisResult.Entities?.Select(j => j.Entity);
+            await context.PostAsync($"Aguarde um momento enquanto eu obtenho os projetos!");
+
+
+            context.Done<string>(null);
+        }
+    }
+}
